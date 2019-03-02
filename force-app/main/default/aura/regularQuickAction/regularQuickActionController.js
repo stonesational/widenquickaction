f595d637258fcd38d8f9b54bb11b81007480a8e3
jason.stone <jason.stone@salesforce.com>
2019-03-01 22:20:07 -0800

    First commit

---
+++ force-app/main/default/aura/regularQuickAction/regularQuickActionController.js
@@ -0,0 +1,20 @@
+({
+    doInit : function(component) {
+        $A.createComponent(
+            "c:wideQuickAction",{},
+            function(content, status){
+                if (status === "SUCCESS"){
+                    component.find("overlayLib").showCustomModal({
+                        header: "I AM WIDE",
+                        body : content,
+                        cssClass: "slds-modal_large",
+                        showCloseButton: true
+                    }).then(function(overlay) { 
+                        $A.get("e.force:closeQuickAction").fire();
+                    });
+                    
+                } else alert("Error: "+status);
+            }
+        );
+    }
+})
---
+++ force-app/main/default/aura/wideQuickAction/wideQuickActionController.js
@@ -0,0 +1,6 @@
+({
+    doInit : function(component) {
+        console.log('STONE: WIDE QuickAction INIT');
+       
+    }
+})
